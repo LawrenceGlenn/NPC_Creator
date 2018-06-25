0c9c0e42a6427c410ffa84285a300967e01c78aa
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2018-06-25 12:04:02 -0700

    added dice class and fixed typo in npc show page

--- NPC_Details/app/helpers/npcs_helper.rb
+++ NPC_Details/app/helpers/npcs_helper.rb
@@ -2,6 +2,7 @@ module NpcsHelper
 
   def inchesToFeet(inch)
     #{}"#{inch/12} feet #{inch%12} inches"
+    Dice.new.roll(1,8)
   end
 
 
--- NPC_Details/app/models/application_record.rb
+++ NPC_Details/app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+class Dice
+
+  def roll(num, die)
+    sum = num.times { rand(die)+1 }
+  end
+
+end
