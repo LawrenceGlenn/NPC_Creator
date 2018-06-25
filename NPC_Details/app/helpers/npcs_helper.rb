e89ded411d109a76a0bffe43f48800b5a3b97ce9
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2018-06-24 23:47:59 -0700

    added inches to feet helper for height and random level / choose from weighted hash for random level selection

--- NPC_Details/app/helpers/npcs_helper.rb
+++ NPC_Details/app/helpers/npcs_helper.rb
@@ -1,2 +1,25 @@
 module NpcsHelper
+
+  def inchesToFeet(inch)
+    "#{inch/12} feet #{inch%12} inches"
+  end
+
+  def randomLevel
+    levels = { }
+    val=1
+    (20).downto(1) do |key|
+      levels[key] = val
+      val = val*2
+    end
+    choose_weighted(levels)
+  end
+
+  def choose_weighted(weighted)
+    sum = weighted.inject(0) { |sum, item_and_weight| sum += item_and_weight[1] }
+    target = rand(sum)
+    weighted.each do |item, weight|
+      return item if target <= weight
+      target -= weight
+    end
+  end
 end
