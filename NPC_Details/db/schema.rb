93a2c768d5ea630fe9de9c00beb4709f710ae1ea
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2019-01-09 21:36:02 -0800

    created city sccafolding

--- NPC_Details/db/schema.rb
+++ NPC_Details/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108070410) do
+ActiveRecord::Schema.define(version: 20190110034802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "pop"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "npcs", force: :cascade do |t|
     t.string   "name"
