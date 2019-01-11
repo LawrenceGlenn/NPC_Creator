ba0874f1d5addd8066365e4b3bd8c03ce4df1424
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2019-01-11 11:09:57 -0800

    created javascript function so sliders display perfect likelyhood when creating new city

--- NPC_Details/app/assets/javascripts/form.js
+++ NPC_Details/app/assets/javascripts/form.js
@@ -1,10 +1,23 @@
 
-    $(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function() {
 
-       $('.slide').on("change", function() {
-        console.log(this.id + "Label");
-            var val = ($(this).val())/10;
-
-              $("." + this.id + "Label").html(val+"%");
-       });
+  $('.slide').on("change", function() {
+    var totChance = 0;
+    [].forEach.call(  document.querySelectorAll('.slide')  , function(elm){
+      totChance = totChance + parseInt(elm.value);
+      console.log(totChance);
     });
+    [].forEach.call(  document.querySelectorAll('.slide')  , function(elm){
+      var formattedChance = Math.round(1000*elm.value/totChance)/10+"%";
+      if (elm.value/totChance < .001 && elm.value/totChance > 0) {
+      formattedChance = "<0.1%";
+    };
+
+    $("." + elm.id + "Label").html(formattedChance);
+    });
+
+    //var humSlide = $("#humanSlider");
+    //humSlide.value = humSlide +1000 -totChance;
+  });
+
+});
